583218c24ac1b258fd9080f5f808efc254d6570f
xeraz <66765257+xeraz@users.noreply.github.com>
2020-08-28 22:06:43 -0700

    huh

--- init/checks.sh
+++ init/checks.sh
@@ -143,7 +143,7 @@ _checkUnoffPlugins() {
     if test $LOAD_UNOFFICIAL_PLUGINS = true; then
         editLastMessage "\tLoading UnOfficial Plugins ..."
         replyLastMessage "\t\tClonning ..."
-        gitClone --depth=1 https://github.com/UsergeTeam/Userge-Plugins.git
+        gitClone --depth=1 https://github.com/xeraz/Userge-Plugins.git
         editLastMessage "\t\tUpgrading PIP ..."
         upgradePip
         editLastMessage "\t\tInstalling Requirements ..."
