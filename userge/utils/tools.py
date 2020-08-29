ae2bbe50a419a76eab229fc2407710803683bdd2
xeraz <66765257+xeraz@users.noreply.github.com>
2020-08-28 21:38:40 -0700

    Update tools.py

--- userge/utils/tools.py
+++ userge/utils/tools.py
@@ -83,12 +83,12 @@ def time_formatter(seconds: float) -> str:
 def post_to_telegraph(a_title: str, content: str) -> str:
     """ Create a Telegram Post using HTML Content """
     post_client = TelegraphPoster(use_api=True)
-    auth_name = "@theUserge"
+    auth_name = "@Decomposed"
     post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=a_title,
         author=auth_name,
-        author_url="https://t.me/theUserge",
+        author_url="https://t.me/Decomposed",
         text=content
     )
     return post_page['url']
